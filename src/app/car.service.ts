f4e9e12ece8c601f8eab61a93d0c51a7c000fa1f
honchunkin <honchunkin@gmail.com>
2019-07-30 11:02:00 +0800

    get() API

---
+++ src/app/car.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+/** To connect with backend */
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+import { Car } from './car';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarService {
+  baseUrl = 'http://localhost/api';
+  cars: Car[];
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Car[]> {
+    return this.http.get(`${this.baseUrl}/list/list.php`).pipe(
+      map((res) => {
+        this.cars = res['data'];
+        return this.cars;
+    }),
+    catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log(error);
+
+    // return an observable with a user friendly message
+    return throwError('Error! something went wrong.');
+  }
+}
